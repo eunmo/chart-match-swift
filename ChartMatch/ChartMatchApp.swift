54059aa096964786f97b3fca117c0fe22ad105c7
eunmo <eunmo@cs.stanford.edu>
2020-09-24 20:35:55 +0900

    Initial Commit

---
+++ ChartMatch/ChartMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartMatchApp.swift
+//  ChartMatch
+//
+//  Created by Eunmo Yang on 2020/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ChartMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
